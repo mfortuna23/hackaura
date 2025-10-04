238712e141a820d656ab25f5783ce75d2d90c249
rgarcia <costagarcia.r95@gmail.com>
2025-10-04 12:26:43 +0100

    update map with marker

--- front/app.js
+++ front/app.js
@@ -44,13 +44,13 @@ document.getElementById('userForm').addEventListener('submit', async (e) => {
 // Load users when page loads
 loadUsers();
 
-var map = L.map('map').setView([41.14852, -8.61317], 22);
+var map = L.map('map').setView([41.14852, -8.61317], 20);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([41.148, -8.6127]).addTo(map);
+var marker = L.marker([41.14852, -8.61317]).addTo(map);
 
 marker.bindPopup("<b>Where it all started!</b><br>CARALHOOO").openPopup();
