856ebf572459b475c0f12f25e0f767b327262570
mfortuna23 <147384586+mfortuna23@users.noreply.github.com>
2025-10-04 17:29:13 +0100

    ups

--- front/app.js
+++ front/app.js
@@ -42,9 +42,13 @@ document.getElementById('userForm').addEventListener('submit', async (e) => {
 // Load users when page loads
 loadUsers();
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([41.14852, -8.61317], 20);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([41.14852, -8.61317]).addTo(map);
+
+marker.bindPopup("<b>Where it all started!</b><br>CARALHOOO").openPopup();
